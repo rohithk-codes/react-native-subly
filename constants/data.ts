ab8ec89b1b7affe3908ab1302140338a505bb3d4
Rohith krishnan <rohithkrishnan997@gmail.com>
2026-04-27 23:57:20 +0530

    feat: add Home, Subscriptions, settings, and insights  tabs to bottom navbar

---
+++ constants/data.ts
@@ -0,0 +1,8 @@
+import {icons} from "./icons";
+
+export const tabs = [
+    {name: "Home", title:"Home", icon: icons.home},
+    {name: "Subscriptions", title:"Subscriptions", icon: icons.subscriptions},
+    {name: "insights", title:"Insights", icon: icons.insights},
+    {name: "Settings", title:"Settings", icon: icons.setting},
+]
--- constants/icons.ts
+++ constants/icons.ts
@@ -16,7 +16,8 @@ import plus from "@/assets/icons/plus.png";
 import setting from "@/assets/icons/setting.png";
 import spotify from "@/assets/icons/spotify.png";
 import wallet from "@/assets/icons/wallet.png";
-
+import insights from "@/assets/icons/insights.png";
+import subscriptions from "@/assets/icons/subscriptions.png";
 export const icons = {
     home,
     wallet,
@@ -36,6 +37,8 @@ export const icons = {
     github,
     claude,
     canva,
+    insights,
+    subscriptions,
 } as const;
 
 export type IconKey = keyof typeof icons;
