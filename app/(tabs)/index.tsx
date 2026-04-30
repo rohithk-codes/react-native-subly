0c1fd2c917e714b1407e351d08e98a7c2466de65
Rohith krishnan <rohithkrishnan997@gmail.com>
2026-04-30 08:49:59 +0530

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,22 +9,23 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">Welcome to Native</Text>
+      <Text className="text-7xl font-sans-bold text-success">Home</Text>
+    
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4" 
       >
         Go to Onboarding
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Sign-In
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         SignUp
       </Link>
